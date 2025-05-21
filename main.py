4b901770050340fb3fd8c34d2ca0371b9c26633c
elicebsch <eliceb@gmx.de>
2025-05-21 15:35:35 +0200

    fünf

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#Namen in alphabetischer Reihenfolge
+﻿#Namen in alphabetischer Reihenfolge
 print("Adem Jabri ")
 print("andré")
 print("Arwin")
-print("Rafael")
-print("Nidhal")
-
 print("Elía")
+print("Nidhal")
+print("Rafael")
+
+
+
